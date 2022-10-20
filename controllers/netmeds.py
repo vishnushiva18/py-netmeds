a10de46a2f6ae5585e0913d822ad9d10719fcb5b
Vishnu K S <vishnu@csquare.in>
2022-10-21 03:28:41 +0530

    changes

--- controllers/netmeds.py
+++ controllers/netmeds.py
@@ -83,7 +83,7 @@ def netmeds_wecare_lang_create(lang):
     canvas = ImageDraw.Draw(finalImg)
     
     user = user.resize((276, 276))
-    finalImg.paste(user, (116, 1552))
+    finalImg.paste(user, (116, 1540))
     finalImg.paste(frame, (0, 0), frame)
 
     tmpY = 1540
@@ -110,9 +110,61 @@ def netmeds_diwali():
     return {}
 
     
-@app.route('/netmeds/diwali/create/<empid>', methods=['GET'])
+@app.route('/netmeds/diwali/create/<empid>', methods=['GET', 'POST'])
 def netmeds_diwali_create(empid):
-    return render_template('netmeds/diwali-create.html', empid=empid)
+    if request.method == "GET":
+        return render_template('netmeds/diwali-create.html', empid=empid)
+
+    frame = f"/assets/images/diwali-2.png"
+    d = request.json
+    texts = [d['name']]
+    
+    frame = f".{frame}"
+    with app.open_resource(frame, 'rb') as f:
+        frame = f.read()
+
+    fontFile = "./assets/fonts/OpenSans-BoldItalic.ttf"
+    with app.open_resource(fontFile, 'rb') as f:
+        fontFile = f.read()
+
+    font = ImageFont.truetype(io.BytesIO(fontFile), size=34)
+    frame = Image.open(io.BytesIO(frame))
+    user = base64.b64decode(d['user_img'].split(';')[1].split(',')[1])
+    user = Image.open(io.BytesIO(user))
+    exif = None
+    if user._getexif():
+        exif=dict((ExifTags.TAGS[k], v) for k, v in user._getexif().items() if k in ExifTags.TAGS)
+
+    if exif and exif.get('Orientation'):
+        orientation = exif.get('Orientation')
+        if orientation == 8:
+            user=user.rotate(90, expand=True)
+        elif orientation == 3:
+            user=user.rotate(180, expand=True)
+        elif orientation == 6:
+            user=user.rotate(270, expand=True)
+
+    finalImg = Image.new('RGB', frame.size, color = 'white')
+    canvas = ImageDraw.Draw(finalImg)
+    
+    user = user.resize((620, 310))
+    finalImg.paste(user, (250, 1030))
+    finalImg.paste(frame, (0, 0), frame)
+
+    tmpY = 1520
+    for t in texts:
+        canvas.text((380, tmpY), t, font=font, fill='#ffffff')
+        tmpY += 55
+
+
+    bufferedFinal = io.BytesIO()
+    finalImg.save(bufferedFinal, format="JPEG")
+    final_str = bytes("data:image/jpeg;base64,", encoding='utf-8') + base64.b64encode(bufferedFinal.getvalue())
+
+    d['user_img'] = None
+    MONGO_CSQUARE.DB["netmeds_microsite_log"].insert_one(d)
+    return {'img': final_str.decode("utf-8")}
+
 
 @app.route('/netmeds/microsite/update', methods=['POST'])
 def netmeds_microsite_update():
--- imgcreate.py
+++ imgcreate.py
@@ -1,24 +1,24 @@
 from PIL import Image, ImageDraw, ImageFont
 def main():
-    frame = "assets/images/WeCare-Diwali_english.png"
+    frame = "assets/images/diwali-2.png"
     user = "assets/images/user.jpg"
     fontFile = "assets/fonts/OpenSans-BoldItalic.ttf"
 
     font = ImageFont.truetype(fontFile, size=34)
-    texts = ["Vishnu", "C-Square", "9567764045", "kochi"]
+    texts = ["Vishnu"]
 
     frame = Image.open(frame)
     user = Image.open(user)
     finalImg = Image.new('RGB', frame.size, color = 'white')
     canvas = ImageDraw.Draw(finalImg)
 
-    user = user.resize((276, 276))
-    finalImg.paste(user, (116, 1552))
+    user = user.resize((620, 310))
+    finalImg.paste(user, (250, 1030))
     finalImg.paste(frame, (0, 0), frame)
 
-    tmpY = 1540
+    tmpY = 1520
     for t in texts:
-        canvas.text((420, tmpY), t, font=font, fill='#ffffff')
+        canvas.text((380, tmpY), t, font=font, fill='#ffffff')
         tmpY += 55
 
 
